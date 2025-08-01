305c7a23346455b825113218894cafebcc701aa2
matias-llanquiman <mllanquimanmelipil@gmail.com>
2025-08-01 17:34:42 -0400

    fix: correct types in model

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,21 +1,28 @@
 export interface User {
+  id: number;
   name: string;
   password: string;
   email: string;
+}
+
+export interface NewUser {
+  name: string;
+  email: string;
+  password: string;
 }
 
 export interface UserSummary {
+  id: number;
   name: string;
   email: string;
-}
-
-export interface RegisterInput {
-  name: string;
-  email: string;
-  password: string;
 }
 
 export interface LoginInput {
   email: string;
   password: string;
+}
+
+export interface LoginResponse {
+  jwt: string;
+  user: UserSummary;
 }
