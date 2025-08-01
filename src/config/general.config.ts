b26df6463d73aa656a81376c144d593d7c96390d
matias-llanquiman <mllanquimanmelipil@gmail.com>
2025-08-01 11:10:15 -0400

    chore: add api key constant

--- src/config/general.config.ts
+++ src/config/general.config.ts
@@ -5,11 +5,13 @@ dotenv.config();
 interface Config {
   port: number;
   appEnv: string;
+  apiKey: string;
 }
 
 const config: Config = {
   port: Number(process.env.APP_PORT) || 4000,
   appEnv: process.env.APP_ENV || 'development',
+  apiKey: process.env.API_SECRET_KEY || '',
 };
 
 export default config;
