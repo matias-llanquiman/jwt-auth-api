b3b3d9ae8d566fbd8574ff0cbd2c47d74f31bbf2
matias-llanquiman <mllanquimanmelipil@gmail.com>
2025-09-04 10:12:48 -0400

    feat: add auth middleware for routes that we want to be authenticated

---
+++ src/middlewares/auth.middleware.ts
@@ -0,0 +1,24 @@
+import jwt, { JwtPayload } from 'jsonwebtoken';
+import { verifyJwt } from '@src/utils/jwt.util';
+import { HttpError } from '@src/errors/http.error';
+import { Request, Response, NextFunction } from 'express';
+
+export interface TokenRequest extends Request {
+  token: string | JwtPayload;
+}
+
+export const auth = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const token = req.header('Authorization')?.replace('Bearer ', '');
+
+    if (!token) {
+      throw new HttpError('Token not found', 401);
+    }
+
+    const decoded = verifyJwt(token);
+    (req as TokenRequest).token = decoded;
+    next();
+  } catch (error) {
+    throw new HttpError('Invalid or expired token', 403);
+  }
+};
