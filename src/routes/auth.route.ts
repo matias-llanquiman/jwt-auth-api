0d693ca5b4bc887ee1091da5312bf34509bc52eb
matias-llanquiman <mllanquimanmelipil@gmail.com>
2025-09-12 11:25:03 -0300

    docs: add openapi documentation with swagger

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -6,6 +6,65 @@ import {
   userLoginSchema,
 } from '@src/schemas/user.schema';
 
+/**
+ * @openapi
+ * '/auth/register':
+ *   post:
+ *     summary: Create a new user
+ *     tags: [Auth]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - name
+ *               - email
+ *               - password
+ *             properties:
+ *               name:
+ *                 type: string
+ *                 example: John Doe
+ *               email:
+ *                 type: string
+ *                 example: john@example.com
+ *               password:
+ *                 type: string
+ *                 example: 12345678
+ *     responses:
+ *       201:
+ *         description: User registered successfully
+ *       400:
+ *         description: Invalid input
+ * '/auth/login':
+ *   post:
+ *     summary: Sign in user
+ *     tags: [Auth]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - email
+ *               - password
+ *             properties:
+ *               email:
+ *                 type: string
+ *                 example: john@example.com
+ *               password:
+ *                 type: string
+ *                 example: 12345678
+ *     responses:
+ *       201:
+ *         description: User registered successfully
+ *       500:
+ *         description: Invalid input
+
+ */
+
 const router = Router();
 
 router.post(
