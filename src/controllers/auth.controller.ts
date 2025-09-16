c23d173719f6c52d53455e9cffb26f53f4b404d9
matias-llanquiman <mllanquimanmelipil@gmail.com>
2025-09-16 14:49:31 -0300

    feat: add refresh token response with httponly

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -15,8 +15,16 @@ export const authController = {
   login: async (req: Request, res: Response, next: NextFunction) => {
     try {
       const { email, password } = req.body;
-      const { jwt, user } = await authService.login({ email, password });
-      res.status(201).json({ token: jwt, user });
+      const { accessToken, refreshToken, user } = await authService.login({
+        email,
+        password,
+      });
+      res.cookie('refresh-token', refreshToken, {
+        httpOnly: true,
+        secure: true,
+        sameSite: 'none',
+      });
+      res.status(201).json({ accessToken: accessToken, user });
     } catch (error) {
       next(error);
     }
