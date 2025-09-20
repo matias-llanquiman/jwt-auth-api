680d6b9f855f12037c3d23d538d835be05f30467
matias-llanquiman <mllanquimanmelipil@gmail.com>
2025-09-19 23:23:04 -0300

    feat: add function for refresh endpoint

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -20,7 +20,7 @@ export const authController = {
         email,
         password,
       });
-      res.cookie('refresh-token', refreshToken, {
+      res.cookie('refreshToken', refreshToken, {
         httpOnly: true,
         secure: true,
         sameSite: 'none',
@@ -34,10 +34,25 @@ export const authController = {
   getProfile: async (req: Request, res: Response, next: NextFunction) => {
     try {
       const userId = (req as TokenRequest).token!.userId;
-      console.log(userId);
       const user = await authService.getProfile(userId);
       res.status(200).json(user);
     } catch (error) {
+      next(error);
+    }
+  },
+
+  refresh: async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const token = req.cookies.refreshToken;
+      const { accessToken, refreshToken } = await authService.refresh(token);
+      res.cookie('refreshToken', refreshToken, {
+        httpOnly: true,
+        secure: true,
+        sameSite: 'none',
+      });
+      res.status(201).json({ accessToken: accessToken });
+    } catch (error) {
+      res.clearCookie('refreshToken');
       next(error);
     }
   },
