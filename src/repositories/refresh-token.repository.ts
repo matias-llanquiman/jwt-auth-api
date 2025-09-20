04cbfb7faebb0295b895cc4cfe0fba79620262b2
matias-llanquiman <mllanquimanmelipil@gmail.com>
2025-09-19 23:21:10 -0300

    feat: add function for select and delete refresh token from db

--- src/repositories/refresh-token.repository.ts
+++ src/repositories/refresh-token.repository.ts
@@ -5,6 +5,15 @@ import {
 } from '@src/types/refresh-token.type';
 
 export const refreshTokenRepository = {
+  findByUserId: async (userId: number): Promise<string> => {
+    const sql = `SELECT refresh_token FROM refresh_tokens WHERE user_id = $1`;
+    const result = await pool.query(sql, [userId]);
+    return result.rows[0] || null;
+  },
+  deleteByUserId: async (userId: number) => {
+    const sql = `DELETE FROM refresh_tokens WHERE user_id = $1`;
+    await pool.query(sql, [userId]);
+  },
   create: async (token: RefreshTokenSummary): Promise<RefreshToken> => {
     const sql = `INSERT INTO refresh_tokens(user_id, refresh_token, expires_at) values($1, $2, $3) RETURNING id, user_id, refresh_token, expires_at, created_at`;
     const result = await pool.query(sql, [
