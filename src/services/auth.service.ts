879721f340b15b1ff9dcfbc88ff0239eaef098c0
matias-llanquiman <mllanquimanmelipil@gmail.com>
2025-09-19 23:21:54 -0300

    feat: add function for refresh endpoint

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -2,14 +2,20 @@ import {
   LoginInput,
   LoginResponse,
   NewUser,
+  RefreshResponse,
   UserSummary,
 } from '@src/types/user.type';
 import { userRepository } from '@src/repositories/user.repository';
 import { HttpError } from '@src/errors/http.error';
 import { hashCompare, hashPassword } from '@src/utils/hash.util';
-import { generateAuthJwt, generateRefreshJwt } from '@src/utils/jwt.util';
+import {
+  generateAuthJwt,
+  generateRefreshJwt,
+  verifyJwt,
+} from '@src/utils/jwt.util';
 import { RefreshTokenSummary } from '@src/types/refresh-token.type';
 import { refreshTokenRepository } from '@src/repositories/refresh-token.repository';
+import { TokenRequest } from '@src/middlewares/auth.middleware';
 
 export const authService = {
   register: async (data: NewUser): Promise<UserSummary> => {
@@ -58,5 +64,40 @@ export const authService = {
       throw new HttpError('User not found', 401);
     }
     return user;
+  },
+
+  refresh: async (refreshToken: string): Promise<RefreshResponse> => {
+    const inputRefreshToken = refreshToken;
+
+    if (!inputRefreshToken) {
+      throw new HttpError('Unauthorized', 401);
+    }
+
+    const decoded = verifyJwt(inputRefreshToken, 'REFRESH');
+    const userId = (decoded as TokenRequest).token!.userId;
+
+    const oldRefreshToken = await refreshTokenRepository.findByUserId(userId);
+
+    if (oldRefreshToken !== inputRefreshToken) {
+      throw new HttpError('Invalid Refresh Token', 401);
+    }
+
+    await refreshTokenRepository.deleteByUserId(userId);
+
+    const accessJwt = generateAuthJwt(userId);
+    const refreshJwt = generateRefreshJwt(userId);
+    let now = new Date();
+    now.setDate(now.getDate() + 7);
+    const expiresAt = now;
+
+    const refreshTokenData: RefreshTokenSummary = {
+      userId: userId,
+      refreshToken: refreshJwt,
+      expiresAt: expiresAt,
+    };
+
+    await refreshTokenRepository.create(refreshTokenData);
+
+    return { accessToken: accessJwt, refreshToken: refreshJwt };
   },
 };
