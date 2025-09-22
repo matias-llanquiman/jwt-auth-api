1d919749d5a75a66f517821f4283e1cd2cedbe9c
matias-llanquiman <mllanquimanmelipil@gmail.com>
2025-09-22 16:00:14 -0300

    chore: install dependencie cookie parser

--- src/app.ts
+++ src/app.ts
@@ -3,10 +3,12 @@ import { errorHandler } from '@src/middlewares/error.middleware';
 import router from '@src/routes/auth.route';
 import swaggerUI from 'swagger-ui-express';
 import { swaggerSpec } from '@src/docs/swagger';
+import cookieParser from 'cookie-parser';
 
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
 app.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerSpec));
 app.use('/auth', router);
 app.use(errorHandler);
